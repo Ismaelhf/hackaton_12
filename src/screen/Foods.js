3a5547b086e62802ac0fa43bccff6021433b8c04
Ismael Huamani Franco <ismaelhf.88@gamil.com>
2020-12-28 18:10:47 -0500

    boton reservar

--- src/screen/Foods.js
+++ src/screen/Foods.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {StyleSheet, View, Image, Text} from 'react-native';
+import {StyleSheet, View, Image, Text, TouchableOpacity} from 'react-native';
 
-const Foods = ({route}) => {
+const Foods = ({route, navigation}) => {
   const item = route.params.item.item;
   // console.warn(item.title);
   return (
@@ -19,6 +19,15 @@ const Foods = ({route}) => {
         <Text style={styles.title}>{item.title}</Text>
 
         <Text style={styles.desp}>{item.description}</Text>
+      </View>
+      <View style={styles.btnReserve}>
+        <TouchableOpacity
+          style={styles.btnReservation}
+          onPress={() => navigation.navigate('Reservation', {item: {item}})}>
+          <View>
+            <Text style={styles.btnText}>Reserva</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -43,6 +52,22 @@ const styles = StyleSheet.create({
   },
   desp: {
     textAlign: 'justify',
+  },
+  ///////////
+  btnReserve: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  btnReservation: {
+    backgroundColor: '#007ACC',
+    width: 100,
+    height: 30,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  btnText: {
+    color: '#fff',
+    textAlign: 'center',
   },
 });
 
--- src/screen/Housing.js
+++ src/screen/Housing.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import {StyleSheet, View, Image, Text, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 
-const Housing = ({route}) => {
+const Housing = ({route, navigation}) => {
   const item = route.params.item.item;
   // console.warn(item.title);
   return (
@@ -19,6 +26,15 @@ const Housing = ({route}) => {
         <Text style={styles.title}>{item.title}</Text>
 
         <Text style={styles.desp}>{item.description}</Text>
+      </View>
+      <View style={styles.btnReserve}>
+        <TouchableOpacity
+          style={styles.btnReservation}
+          onPress={() => navigation.navigate('Reservation', {item: {item}})}>
+          <View>
+            <Text style={styles.btnText}>Reserva</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -43,6 +59,22 @@ const styles = StyleSheet.create({
   },
   desp: {
     textAlign: 'justify',
+  },
+  ///////////
+  btnReserve: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  btnReservation: {
+    backgroundColor: '#007ACC',
+    width: 100,
+    height: 30,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  btnText: {
+    color: '#fff',
+    textAlign: 'center',
   },
 });
 
